2cb80967f9fbb2b9c4a5ebf81277a09ce10a3c6c
slechenne <sandro@sandrolechenne.ch>
2020-02-19 17:07:09 +0100

    wave

--- js/main.js
+++ js/main.js
@@ -9,3 +9,24 @@ $(document).ready(function(){
     $("nav").toggleClass("open");
   });
 });
+
+
+
+
+
+// For jQuery
+var myWave = $('#wave1').wavify({
+  height: 40,
+  bones: 4,
+  amplitude: 120,
+  // color: '#FFF',
+  speed: .13
+});
+
+var myWave2 = $('#wave2').wavify({
+  height: 40,
+  bones: 3,
+  amplitude: 120,
+  color: '#FFF',
+  speed: .13
+});
