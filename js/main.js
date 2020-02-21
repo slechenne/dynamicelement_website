25b1e0c958ecba5615d40c1d3d57c207ea153b78
slechenne <sandro@sandrolechenne.ch>
2020-02-21 11:16:40 +0100

    sdlkjf

--- js/main.js
+++ js/main.js
@@ -14,16 +14,32 @@ $(document).ready(function(){
 
 
 
-// For jQuery
+
 var myWave = $('#wave1').wavify({
   height: 40,
   bones: 4,
   amplitude: 120,
   // color: '#FFF',
-  speed: .13
+  speed: .11
 });
 
 var myWave2 = $('#wave2').wavify({
+  height: 40,
+  bones: 3,
+  amplitude: 120,
+  color: '#FFF',
+  speed: .11
+});
+
+var myWave3 = $('#wave3').wavify({
+  height: 40,
+  bones: 5,
+  amplitude: 120,
+  // color: '#FFF',
+  speed: .12
+});
+
+var myWave4 = $('#wave4').wavify({
   height: 40,
   bones: 3,
   amplitude: 120,
