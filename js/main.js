7f9cfdf54cad9f101e173b0587378ccf5e6080f5
slechenne <sandro@sandrolechenne.ch>
2020-02-25 16:55:07 +0100

    hadjfklajdfjljlsdölkajsdfasylkpsdölpkjasdflkasdfk f

--- js/main.js
+++ js/main.js
@@ -46,3 +46,21 @@ var myWave4 = $('#wave4').wavify({
   color: '#FFF',
   speed: .13
 });
+
+
+
+var swiper = new Swiper('.swiper-container', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+});
